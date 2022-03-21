f54d9970ab9975917d269fe22ecdd156cac508ab
Sarathskb <21sarathkumar2000@gmail.com>
2022-03-21 20:10:01 +0530

    elif py

---
+++ if.py
@@ -0,0 +1,9 @@
+i = int(input("Enter the number : "))
+if (i==10):
+    print("i is 10")
+elif (i==30):
+    print("i is 30")
+elif (i==40):
+    print("i is 40")
+else:
+    print("i is not present")
